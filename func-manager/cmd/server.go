51a0fee2279376a9b560cef2575b41b6d58ff994
xiangxu126 <xiangxu527@gmail.com>
2024-04-13 00:14:43 +0800

    在根目录下创造文件实现函数持久化存储

--- func-manager/cmd/server.go
+++ func-manager/cmd/server.go
@@ -9,11 +9,13 @@ import (
 	"golang.org/x/exp/maps"
 	"google.golang.org/grpc"
 	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"math/rand/v2"
 	"net"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"syscall"
 )
 
@@ -21,8 +23,9 @@ var (
 	localRpcAddr    string
 	coordinatorAddr string
 	funcManagerName string
-	funcYaml        map[string]*function //通过函数名索引函数，todo：考虑持久化
+	funcYaml        = make(map[string]*function) //通过函数名索引函数
 	ExitCh          = make(chan int)
+	functionDir     = "." + string(os.PathSeparator) + "function"
 )
 
 type function struct {
@@ -34,7 +37,7 @@ type function struct {
 
 var Cmd = &cobra.Command{
 	Use:   "gateway",
-	Short: `初始化gateway程序`,
+	Short: `初始化func-manager程序`,
 	//本函数用于执行命令并返回错误
 	RunE: func(cmd *cobra.Command, args []string) error {
 		funcManagerName = strconv.Itoa(int(rand.Uint32()))
@@ -59,6 +62,64 @@ func init() {
 	log.SetOutput(os.Stdout)
 	log.SetLevel(log.InfoLevel)
 	log.SetReportCaller(true)
+
+	//创建保存function的文件
+	if _, err := os.Stat(functionDir); os.IsNotExist(err) {
+		// 不存在则创建
+		err := os.Mkdir(functionDir, 0755)
+		if err != nil {
+			log.Fatalln("create function folder err:", err)
+		}
+		fmt.Println("Folder created successfully:", functionDir)
+	} else {
+		//存在则载入
+		files, err := ioutil.ReadDir(functionDir)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		// 循环处理每个文件
+		for _, file := range files {
+			fileName := file.Name()
+			if !file.IsDir() && strings.HasSuffix(fileName, ".yaml") {
+				// 解析文件名
+				parts := strings.Split(strings.TrimSuffix(fileName, ".yaml"), "-")
+
+				// 初始化标签和注释
+				name := ""
+				label := ""
+				annotation := ""
+
+				// 如果文件名中包含足够的部分，则解析标签和注释
+				name = parts[0]
+				if len(parts) >= 2 {
+					label = parts[1]
+				}
+				if len(parts) >= 3 {
+					annotation = parts[2]
+				}
+				// 读取文件内容
+				fileContent, err := ioutil.ReadFile(functionDir + string(os.PathSeparator) + fileName)
+				if err != nil {
+					log.Errorln("Error reading yaml file:", fileName, err)
+					continue
+				}
+
+				// 文件内容为yaml编码成的字符串
+				fileContentStr := string(fileContent)
+				// 构建函数结构体
+				funcStruct := &function{
+					Name:       name,
+					Label:      label,
+					Annotation: annotation,
+					Document:   fileContentStr,
+				}
+
+				// 添加到函数映射中
+				funcYaml[name] = funcStruct
+			}
+		}
+	}
 }
 
 // Run 提供给顶层用于启动cobra根命令
@@ -114,7 +175,14 @@ func (s *rpcServerProcess) Create(ctx context.Context, req *proto.CreateReq) (*p
 		Annotation: req.Annotations,
 		Document:   req.Document,
 	}
-	funcYaml[newFunc.Name] = newFunc
+
+	functionPath := functionDir + string(os.PathSeparator) + buildName(newFunc) //此处的文件名使用name-label-annotation的格式构造
+	funcYaml[req.Request.Name] = newFunc
+	err := os.WriteFile(functionPath, []byte(req.Document), 0666)
+	if err != nil {
+		log.Errorln("save yaml err:", err)
+		return nil, fmt.Errorf("save yaml err:%v", err)
+	}
 	return &proto.CreateResp{
 		RequestId:        req.Request.RequestId,
 		StatusCode:       0,
@@ -136,9 +204,14 @@ func (s *rpcServerProcess) Get(ctx context.Context, req *proto.GetReq) (*proto.G
 
 }
 func (s *rpcServerProcess) Delete(ctx context.Context, req *proto.DeleteReq) (*proto.DeleteResp, error) {
-	_, ok := funcYaml[req.Request.Name]
+	f, ok := funcYaml[req.Request.Name]
 	if !ok {
 		return nil, fmt.Errorf("functione not found")
+	}
+	err := os.Remove(functionDir + string(os.PathSeparator) + buildName(f))
+	if err != nil {
+		log.Errorln("function delete err:", err)
+		return nil, fmt.Errorf("function delete err:%v", err)
 	}
 	delete(funcYaml, req.Request.Name)
 	return &proto.DeleteResp{
@@ -162,4 +235,17 @@ func (s *rpcServerProcess) List(ctx context.Context, req *proto.ListReq) (*proto
 		StatusCode: 0,
 		List:       string(yamlData),
 	}, nil
+}
+func buildName(f *function) string {
+	res := f.Name
+	if f.Label != "" {
+		res += "-" + f.Label
+	}
+	if f.Annotation != "" {
+		if f.Label == "" {
+			res += "-"
+		}
+		res += "-" + f.Annotation
+	}
+	return res
 }
